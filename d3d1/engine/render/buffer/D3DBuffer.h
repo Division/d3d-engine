58fb493b2c6966990dee20d13ae50f123fefa210
Division <pmujik@gmail.com>
2019-02-17 22:06:15 +0200

    Deleted odd code

--- d3d1/engine/render/buffer/D3DBuffer.h
+++ d3d1/engine/render/buffer/D3DBuffer.h
@@ -9,7 +9,6 @@ class ID3DContextProvider;
 
 class D3DBuffer : public MemoryBuffer {
 public:
- // D3DBuffer(GLenum target, GLenum usage, unsigned int fixedSize = 0);
 	D3DBuffer(ID3DContextProvider *provider, D3D11_BIND_FLAG bindFlag, D3D11_USAGE usage, int32_t fixedSize = 0);
 	~D3DBuffer() override;
 
@@ -28,43 +27,4 @@ protected:
 	int32_t _fixedSize;
 
 	unsigned int _bufferAllocatedSize = 0;
-
-  /*
-  virtual void bind();
-  virtual void unbind();
-  void upload() override;
-  GLuint vbo() { return _vbo; }
-
-protected:
-  unsigned int _bufferAllocatedSize = 0;
-  GLuint _vbo = 0;
-  GLenum _target;
-  GLenum _usage;
-  unsigned int _fixedSize;
-  void _recreateBuffer();
-
-  */
 };
-
-
-//typedef std::shared_ptr<VertexBufferObject> VertexBufferObjectPtr;
-
-/*
-class SwappableVertexBufferObject : public SwappableBufferObject<VertexBufferObject> {
-public:
-  SwappableVertexBufferObject(GLenum target, GLenum usage, unsigned int fixedSize = 0, unsigned int count = 2)
-      : SwappableBufferObject<VertexBufferObject>(count), _target(target), _usage(usage), _fixedSize(fixedSize) {
-    _createBuffers();
-  }
-
-protected:
-  std::shared_ptr<VertexBufferObject> createBuffer() override;
-
-private:
-  GLenum _target;
-  GLenum _usage;
-  unsigned int _fixedSize;
-};
-
-typedef std::shared_ptr<SwappableVertexBufferObject> SwappableVertexBufferObjectPtr;
-*/
