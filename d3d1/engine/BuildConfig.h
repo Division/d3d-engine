abdc6c14ae4a854ad7c602041a9656ce1e7afb4b
Division <pmujik@gmail.com>
2019-05-08 22:20:58 +0100

    Conditional compilation for error dump

--- d3d1/engine/BuildConfig.h
+++ d3d1/engine/BuildConfig.h
@@ -1,9 +1,11 @@
 ﻿#if defined ENV_PRODUCTION
 #define ENGINE_DIRECTX_DEBUG false
+#define CRASHDUMP_ENABLED true
 #else
 #define ENGINE_DIRECTX_DEBUG true
+#define CRASHDUMP_ENABLED false
 #endif
 
 // It's being auto incremented by the pre build script
-#define BUILD_COMMIT_HASH L"6ad78f4"
+#define BUILD_COMMIT_HASH L"1756c7d"
 
